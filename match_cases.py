eabc3cc5e1dd7fee9b78254837a623dd4b51a29e
aphankosol <aphisit.phankosol@agoda.com>
2022-03-01 15:05:15 +0700

    fix matching case function

--- match_cases.py
+++ match_cases.py
@@ -4,15 +4,21 @@ from typing import List
 def is_match_case(candidate_word: str, labeled_word: str, case: List[str]) -> bool:
     result = True
     for i, c in enumerate(case):
+        target_char = labeled_word[i]
         if c == '🟩':
-            result = result and (labeled_word[i] == candidate_word[i])
+            result = result and (target_char == candidate_word[i])
 
         elif c == '⬜':
-            result = result and (labeled_word[i] not in candidate_word)
-
+            sub_result = True
+            for j in range(len(candidate_word)):
+                if candidate_word[j] == target_char:
+                    if case[j] != '🟩':
+                        sub_result = False
+                        break
+            result = result and sub_result
         elif c == '🟨':
             result = result and (
-                    labeled_word[i] in labeled_word and labeled_word[i] != candidate_word[i])
+                    target_char in labeled_word and target_char != candidate_word[i])
         else:
             raise ValueError(f'Unknown charactor {c} len={len(c)}')
         if not result:
@@ -29,6 +35,7 @@ def filter_words_by_case(possible_words_answer, played_word, case: List[str]) ->
 
 
 def test_is_match_case():
-    assert is_match_case('async', 'again', ['🟩', '⬜', '⬜', '🟨', '⬜']) == True
-    assert is_match_case('async', 'again', ['🟩', '⬜', '🟨', '🟨', '⬜']) == True
+    assert is_match_case('again', 'async', ['🟩', '⬜', '⬜', '🟨', '⬜']) == True
+    assert is_match_case('again', 'async', ['🟩', '⬜', '🟨', '🟨', '⬜']) == True
     assert is_match_case('abccd', 'ccccc', ['🟨', '🟨', '🟩', '🟩', '🟨']) == True
+    assert is_match_case('rupee', 'tepee', ['⬜', '⬜', '🟩', '🟩', '🟩']) == True
